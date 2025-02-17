bf1aeef921bb3f8ad897df32ec298c8819517728
OtaPul <otto.pulkkinen@student.lut.fi>
2025-02-17 22:15:08 +0200

    Update Violin.java

--- Violin.java
+++ Violin.java
@@ -1,5 +1,12 @@
+package main;
+
 public class Violin extends StringInstrument {
     public Violin(String manufacturer, int price) {
         super(manufacturer, price, 4);
+    }
+
+    @Override
+    public String getDetails() {
+        return "Valmistaja: " + manufacturer + ", Hinta: " + price + "e, Kieliä: 4 (viulu)";
     }
 }
