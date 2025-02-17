470497a28a4c96f68dae2e2a1e1dcf15929018ae
OtaPul <otto.pulkkinen@student.lut.fi>
2025-02-17 22:14:55 +0200

    Update Guitar.java

--- Guitar.java
+++ Guitar.java
@@ -1,5 +1,12 @@
+package main;
+
 public class Guitar extends StringInstrument {
     public Guitar(String manufacturer, int price) {
         super(manufacturer, price, 6);
+    }
+
+    @Override
+    public String getDetails() {
+        return "Valmistaja: " + manufacturer + ", Hinta: " + price + "e, Kieliä: 6 (kitara)";
     }
 }
