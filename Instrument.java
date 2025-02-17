6cc58d052f0c59703d7cfe318384f349f5863198
OtaPul <otto.pulkkinen@student.lut.fi>
2025-02-17 22:07:24 +0200

    Create Instrument.java

---
+++ Instrument.java
@@ -0,0 +1,13 @@
+public abstract class Instrument {
+    protected String manufacturer;
+    protected int price;
+
+    public Instrument(String manufacturer, int price) {
+        this.manufacturer = manufacturer;
+        this.price = price;
+    }
+
+    public String getDetails() {
+        return "Valmistaja: " + manufacturer + ", Hinta: " + price + "e";
+    }
+}
